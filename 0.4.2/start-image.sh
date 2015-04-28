772690d45db51554803c464c26ef289cf773cf13
Graham Bevan <gbevan@csc.com>
2015-04-28 09:59:28 +0100

    allow execute of an injected start image hook, for layered images

--- 0.4.2/start-image.sh
+++ 0.4.2/start-image.sh
@@ -6,5 +6,13 @@ echo `date` "Starting SSHD"
 echo `date` "Starting MongoDB"
 /usr/bin/mongod --config /etc/mongod.conf --fork
 
+# Test for and execute a hook script (to be past by docker images layered
+# on top of this image.
+if [ -x /start-image-hook.sh ]
+then
+  echo `date` "Running start-image-hook"
+  /start-image-hook.sh
+fi
+
 echo `date` "Starting MEAN.IO Appserver"
 su - mean -c "cd appserver && NODE_ENV=production forever -w server.js"
