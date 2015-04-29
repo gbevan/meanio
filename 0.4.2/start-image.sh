c70d9aa5138ec4ecb126ebd193d9f4a4961dab24
Graham Bevan <gbevan@csc.com>
2015-04-29 11:01:41 +0100

    shutdown mongod

--- 0.4.2/start-image.sh
+++ 0.4.2/start-image.sh
@@ -7,7 +7,7 @@ echo `date` "Running mongod repair"
 /usr/bin/mongod --repair --config /etc/mongod.conf
 
 echo `date` "Starting MongoDB"
-/usr/bin/mongod --config /etc/mongod.conf --fork --smallfiles --nojournal
+/usr/bin/mongod --config /etc/mongod.conf --fork --smallfiles
 
 # Test for and execute a hook script (to be past by docker images layered
 # on top of this image.
@@ -17,5 +17,8 @@ then
   . /start-image-hook.sh
 fi
 
-echo `date` "Starting MEAN.IO Appserver"
-su - mean -c "cd appserver && grunt cssmin && grunt uglify && NODE_ENV=production forever -w -c 'node ${NODEARGS}' server.js ${MEANARGS}"
+echo `date` "Preparing MEAN.IO Appserver"
+su - mean -c "cd appserver && grunt cssmin && grunt uglify"
+
+echo `date` "Starting MEAN.IO Appserver. NODEARGS:${NODEARGS} MEANARGS:${MEANARGS}"
+su - mean -c "cd appserver && NODE_ENV=production forever -w -c 'node ${NODEARGS}' server.js ${MEANARGS}"
